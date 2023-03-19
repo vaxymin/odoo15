353ca57c1dcf5c94131c7bc96f491b218191d0f7
vaxymin <aminatkhalamagamedova@gmail.com>
2023-03-19 22:12:45 +0600

    update

---
+++ custom-modules/om_shrek_boloto/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Boloto Management',
+    'version': '1.0.0',
+    'category': 'Boloto Management',
+    'sequence': -100,
+    'summary': 'Boloto management system',
+    'description': """Shrek Management System""",
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'application': True,
+    'installable': True,
+}
---
+++ custom-modules/om_shrek_boloto/models/clients.py
@@ -0,0 +1,6 @@
+from odoo import api, fields, models
+
+
+class BolotoClient(models.Model):
+    _name = "boloto.client"
+    _description = "Boloto Client"
